06408e66c31779a03ad0822fb167b5dae435c4f5
ManurangaJay <manurangajayawardhana@gmail.com>
2024-10-18 10:24:12 +0530

    A custom business logic was added to post a review using MongoTemplate

--- movieApp/src/main/java/com/manujay/movieApp/service/ReviewService.java
+++ movieApp/src/main/java/com/manujay/movieApp/service/ReviewService.java
@@ -1,4 +1,8 @@
 package com.manujay.movieApp.service;
 
+import com.manujay.movieApp.document.Review;
+import com.manujay.movieApp.repository.ReviewRepository;
+
 public interface ReviewService {
+    public Review createReview(String reviewBody, String imdbId);
 }
--- movieApp/src/main/java/com/manujay/movieApp/service/impl/ReviewServiceImpl.java
+++ movieApp/src/main/java/com/manujay/movieApp/service/impl/ReviewServiceImpl.java
@@ -1,8 +1,32 @@
 package com.manujay.movieApp.service.impl;
 
+import com.manujay.movieApp.document.Movie;
+import com.manujay.movieApp.document.Review;
+import com.manujay.movieApp.repository.ReviewRepository;
 import com.manujay.movieApp.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    @PostMapping
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
 }
